3b08b79cb262c9bb617f644623b0d3233a4a237c
Victor220311 <victorfrancodb@gmail.com>
2025-09-28 18:51:49 +0000

    ejercicio 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+import re
+import random
+
+# Ejercicio 1: Decodificador de Mensajes Secretos
+def decodificar_mensaje(mensaje_cifrado):
+    """
+    Decodifica un mensaje invertido eliminando caracteres especiales
+    """
+    # Voltear la cadena
+    mensaje_volteado = mensaje_cifrado[::-1]
+    
+    # Eliminar caracteres no alfabéticos usando expresión regular
+    mensaje_limpio = re.sub(r'[^a-zA-Z\s]', '', mensaje_volteado)
+    
+    return mensaje_limpio.strip()
+
